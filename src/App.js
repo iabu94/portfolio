0be759244a7f00e2fe39ef3111b4c65f3b25e160
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-05-19 02:10:42 +0530

    Base

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      <h4>Abdullah Mansoor</h4>
+    </div>
   );
 }
 
